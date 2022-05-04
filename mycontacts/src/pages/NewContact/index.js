b4645d4662219c18978e09795bd5c3f45f82e465
Eduardo Pacheco Celeste <eduardopc91@hotmail.com>
2022-05-04 02:20:32 -0300

    myContacts: input, select and button componenets

--- mycontacts/src/pages/NewContact/index.js
+++ mycontacts/src/pages/NewContact/index.js
@@ -1,5 +1,21 @@
 import React from 'react';
 
+import PageHeader from '../../components/PageHeader';
+import Input from '../../components/Input';
+import Select from '../../components/Select';
+import Button from '../../components/Button';
+
 export default function NewContact() {
-  return <div>NewContact</div>;
+  return (
+    <div>
+      <PageHeader title="Novo Contato" />
+      <Input type="text" placeholder="Nome" />
+
+      <Select>
+        <option value="123">Teste</option>
+      </Select>
+      <Button>Salvar alterações</Button>
+      <Button disabled>Salvar alterações</Button>
+    </div>
+  );
 }
