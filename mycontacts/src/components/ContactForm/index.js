a89ad832531e56b9f61fb15bc6d8837fdb022dcf
Eduardo Pacheco <eduardo.celeste@pipefy.com>
2022-06-19 00:59:56 -0300

    refactor: form validations

--- mycontacts/src/components/ContactForm/index.js
+++ mycontacts/src/components/ContactForm/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 import Input from '../Input';
@@ -6,28 +6,97 @@ import Select from '../Select';
 import Button from '../Button';
 import FormGroup from '../FormGroup';
 
+import isEmailValid from '../../utils/isEmailValid';
+import useErrors from '../../hooks/useErrors';
+
 import * as S from './styles';
 
 export default function ContactForm({ buttonLabel }) {
+  const [name, setName] = useState(''); // controlled components
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [category, setCategory] = useState('');
+
+  const { setError, removeError, getErrorByFieldValue } = useErrors();
+  // const phoneInput = useRef(null); // uncontrolled component
+
+  // function handlePhoneInput() {
+  //   console.log(phoneInput.current.value);
+  // }
+
+  const handleName = (event) => {
+    setName(event.target.value);
+
+    if (!event.target.value) {
+      setError({ field: 'name', message: 'Campo nome é obrigatório' });
+    } else {
+      removeError('name');
+    }
+  };
+
+  const handleEmail = (event) => {
+    setEmail(event.target.value);
+
+    if (event.target.value && !isEmailValid(event.target.value)) {
+      setError({ field: 'mail', message: 'E-mail inválido!' });
+    } else {
+      removeError('mail');
+    }
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault(); // evita o refresh da pagina ao realizar o submit
+
+    console.log({
+      name,
+      email,
+      phone,
+      category,
+    });
+  };
+
   return (
-    <S.Form>
-      <FormGroup>
-        <Input type="text" placeholder="Nome" />
+    <S.Form onSubmit={handleSubmit}>
+      {/* <button type="button" onClick={handlePhoneInput}>Show phoneInput Value</button> */}
+      <FormGroup error={getErrorByFieldValue('name')}>
+        <Input
+          error={getErrorByFieldValue('name')}
+          type="text"
+          placeholder="Nome"
+          value={name}
+          onChange={handleName}
+        />
       </FormGroup>
 
-      <FormGroup
-        error="Erro"
-      >
-        <Input type="text" placeholder="E-mail" error />
+      <FormGroup error={getErrorByFieldValue('mail')}>
+        <Input
+          error={getErrorByFieldValue('mail')}
+          type="text"
+          placeholder="E-mail"
+          value={email}
+          onChange={handleEmail}
+        />
       </FormGroup>
 
       <FormGroup>
-        <Input type="text" placeholder="Telefone" />
+        <Input
+          type="text"
+          placeholder="Telefone"
+          value={phone}
+          onChange={(event) => setPhone(event.target.value)}
+          // ref={phoneInput}
+          // onChange={(event) => event.target.value}
+        />
       </FormGroup>
 
       <FormGroup>
-        <Select>
-          <option value="123">Teste</option>
+        <Select
+          value={category}
+          onChange={(event) => setCategory(event.target.value)}
+        >
+          <option value="">Selecione</option>
+          <option value="instagram">Instagram</option>
+          <option value="linkedin">LinkedIn</option>
         </Select>
       </FormGroup>
 
---
+++ mycontacts/src/hooks/useErrors.js
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+
+export default function useErrors() {
+  const [errors, setErrors] = useState([]);
+
+  function setError({ field, message }) {
+    const alreadyExists = errors.find((error) => error.field === field);
+
+    if (alreadyExists) {
+      return;
+    }
+
+    setErrors((prevState) => [...prevState, { field, message }]);
+  }
+
+  function removeError(field) {
+    return setErrors((prevState) =>
+      prevState.filter((error) => error.field !== field));
+  }
+
+  function getErrorByFieldValue(fieldValue) {
+    return errors.find((error) => error.field === fieldValue)?.message;
+  }
+
+  return { setError, removeError, getErrorByFieldValue };
+}
